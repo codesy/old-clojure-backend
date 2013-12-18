02a52dbde89b53c6b7ecac0dbf3fdc7fe9328c61
Edward Cho <zerokarmaleft@gmail.com>
2013-12-18 11:33:50 -0600

    add singular bid resource

--- src/patronage/routes/api.clj
+++ src/patronage/routes/api.clj
@@ -4,15 +4,47 @@
             [liberator.core      :refer [defresource]]
             [patronage.models.db :as    db]))
 
+(defn list-bids [ctx]
+  (json/generate-string (db/get-all-bids)))
+
+(defn create-bid [ctx]
+  (db/create-bid
+   (-> (get-in ctx [:request :body])
+       slurp
+       json/parse-string)))
+
+(defn show-bid
+  [id]
+  (fn [ctx]
+    (let [bid (db/get-bid id)]
+      (when-not (nil? bid)
+        {::bid (json/generate-string bid)}))))
+
+(defn update-bid
+  [id]
+  (fn [ctx]
+    (let [values (-> (get-in ctx [:request :body])
+                     slurp
+                     json/parse-string)]
+      (db/update-bid id
+                     (:url values)
+                     (:offer values)
+                     (:ask values)))))
+
 (defresource bids
   :available-media-types ["application/json"]
   :allowed-methods       [:get :post]
-  :handle-ok             (fn [ctx] (-> (db/get-all-bids)
-                                      json/generate-string))
-  :post!                 (fn [ctx] (db/create-bid
-                                   (-> (get-in ctx [:request :body])
-                                       slurp
-                                       json/parse-string))))
+  :handle-ok             list-bids
+  :post!                 create-bid)
+
+(defresource bid [id]
+  :available-media-types ["application/json"]
+  :allowed-methods       [:get :put]
+  :exists?               (show-bid id)
+  :existed?              (fn [_] (not (nil? (db/get-bid id))))
+  :handle-ok             ::bid
+  :put!                  (update-bid id))
 
 (defroutes api-routes
+  (ANY ["/bids/:id" :id #".*"] [id] (bid id))
   (ANY "/bids" [] bids))
--- test/patronage/routes/api_test.clj
+++ test/patronage/routes/api_test.clj
@@ -17,7 +17,12 @@
                :offer 5.0
                :ask   50.0}]]
     (with-redefs [db/get-all-bids (fn [] bids)
-                  db/create-bid   (fn [bid] {::id 1})]
+                  db/create-bid   (fn [bid] {::id 1})
+                  db/get-bid      (fn [id] (assoc bid :id 1))
+                  db/update-bid   (fn [id url offer ask]
+                                     (merge bid {:url url
+                                                 :offer offer
+                                                 :ask ask}))]
       (testing "bids list route"
         (let [response (-> (request :get "/bids")
                            api-routes)]
@@ -26,6 +31,23 @@
                  (json/generate-string bids)))))
 
       (testing "bids create route"
-        (let [response (-> (request :post "/bids" (json/generate-string bid))
+        (let [response (-> (request :post "/bids"
+                                    (json/generate-string bid))
+                           api-routes)]
+          (is (= (:status response) 201))))
+
+      (testing "bids show route"
+        (let [response (-> (request :get "/bids/1")
+                           api-routes)]
+          (is (= (:status response) 200))
+          (is (= (:body response)
+                 (json/generate-string (assoc bid :id 1))))))
+
+      (testing "bids update route"
+        (let [new-bid {:url   "https://github.com/githubber/gitproject/issues/2"
+                       :offer 10.0
+                       :ask   100.0}
+              response (-> (request :put "/bids/1"
+                                    (json/generate-string new-bid))
                            api-routes)]
           (is (= (:status response) 201)))))))
