0ce673b8684e1e206ca524b4fe5e814b04788bea
Edward Cho <zerokarmaleft@gmail.com>
2014-01-22 11:47:18 -0600

    log full stacktrace when GITHUB_OAUTH_CALLBACK isn't set

--- src/patronage/auth/logging.clj
+++ src/patronage/auth/logging.clj
@@ -1,6 +1,9 @@
 (ns patronage.auth.logging
-  (:require [dire.core :refer [with-handler!]]))
+  (:require [dire.core             :refer [with-handler!]]
+            [patronage.auth.github :refer :all]))
 
 (with-handler! #'github-oauth-callback
   java.net.MalformedURLException
-  (fn [e & args] (println "GITHUB_OAUTH_CALLBACK is not set.")))
+  (fn [e & args]
+    (println "GITHUB_OAUTH_CALLBACK is not set.")
+    (println *err* e)))
